12a92824e5c236944402859ff3f0823521ceb0e8
Monu Rajj <monu2feb2004.com>
2024-02-25 13:31:32 +0530

    Ai trip planner

--- src/components/ItineraryScreen/index.jsx
+++ src/components/ItineraryScreen/index.jsx
@@ -2,7 +2,7 @@ import "./style.css";
 import BrandHeader from "../Common/BrandHeader/index.jsx";
 import Card from "../Common/Card/index.jsx";
 import Button from "../Common/Button/index.jsx";
-import { Link, useLocation } from "react-router-dom";
+import { Link, json, useLocation } from "react-router-dom";
 import useFetchAPI from "../../hooks/useFetchAPI.js";
 import Loading from "../Common/Loading/index.jsx";
 import ErrorScreen from "../ErrorScreen/index.jsx";
@@ -26,6 +26,44 @@ const ItineraryScreen = () => {
 
   useEffect(() => {
     // write a fetch call to get the data from the openAI API, also dont forget to pass prompt as body :)
+    setLoading(true);
+    fetch("https://trip-planner-ai-api.onrender.com/wanderlust", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.success) {
+          try {
+            const responseData = JSON.parse(data.response);
+            if (responseData.success === false) {
+              setError(responseData.error.message);
+            } else if (!responseData?.itinerary) {
+              throw new Error(Object.values(responseData)[0]);
+            } else {
+              setData(responseData);
+            }
+          } catch (error) {
+            setError(error.message);
+          }
+        }
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
 
   const itineraries = data?.itinerary || [];
